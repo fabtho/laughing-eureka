b6f9e3c6867434a758219f25ee321babc2d2c676
Fabian Thommen <fabtho@users.noreply.github.com>
2017-09-14 18:50:38 +0200

    Create ethervault.sol

---
+++ solitity/examples/ethervault/ethervault.sol
@@ -0,0 +1,38 @@
+contract timeVault {
+	struct Member {
+		address addr;
+		uint amount;
+		uint blockNumberDeposited;
+		uint blockWithdrawlNumber;
+	}
+
+	Member[] public members;
+	uint public fundsStored;
+	uint public numDeposits;
+
+	function timeVault() {
+		fundsStored = 0;
+		numDeposits = 0;
+	}
+
+	function sendAssets(uint blockWithDrawlNumber) {
+		Member newMember;
+		newMember.addr = msg.sender;
+		newMember.amount = msg.value;
+		newMember.blockNumberDeposited = block.number;
+		newMember.blockWithdrawlNumber = blockWithDrawlNumber;
+		members[numDeposits] = newMember;
+		numDeposits++;
+		fundsStored = fundsStored + msg.value;
+	}
+
+	function retrieveAssets() { 
+		for (uint i =0; i < numDeposits; ++i) {
+			if (members[i].blockWithdrawlNumber >= block.number) {
+				members[i].addr.send(members[i].amount);
+				numDeposits--;
+				fundsStored = fundsStored - msg.value;
+			}
+		}
+	}
+}
